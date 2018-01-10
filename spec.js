ed0f4257c4b881407a1f01fd2d562101334d4dea
vrgarage <paolo.farinelli@gmail.com>
2018-01-10 16:39:17 +0100

    removed unwanted test

--- spec.js
+++ spec.js
@@ -4,12 +4,6 @@ AsyncVoterApiClient = require('./client')
 
 var client = new AsyncVoterApiClient();
 
-describe('a simple test', function(){
-  it('simpleness', function(){
-    expect(true).to.equal(true)
-  })
-});
-
 describe('testing base URL functionality', function(){
   it('should return the default production api URL', function(){
     expect(client.baseUrl).to.equal('http://api-production.asyncvoter.agileventures.org');
