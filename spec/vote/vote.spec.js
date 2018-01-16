8beb9a30fbd2d4d7592193b64527e0d80e18c506
Brent Vardy <brent.vardy@challengelogic.net>
2018-01-16 14:37:48 +0000

    add tests for getVotesForStory. Fixes #6

--- spec/vote/vote.spec.js
+++ spec/vote/vote.spec.js
@@ -58,3 +58,52 @@ describe('testing creating a vote for a story', function () {
     });
 
 });
+
+describe('testing getting all votes for a story', function () {
+    var newVote = {
+        userId: '@test_user1',
+        size: '1'
+    };
+    var newVoteId;
+
+    before(function (done) {
+        this.timeout(2500);
+        client.setBaseUrl('http://api-test.asyncvoter.agileventures.org');
+        client.createVote(storyId, newVote, function (err, data, response) {
+            newVoteId = data._id;
+            done();
+        });
+    });
+
+    it('should return a code 200', function (done) {
+        client.getVotesForStory(storyId, function (err, data, response) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it('should return a null error', function (done) {
+        client.getVotesForStory(storyId, function (err, data, response) {
+            expect(err).to.equal(null);
+            done();
+        });
+    });
+
+    it('should return an array', function (done) {
+        client.getVotesForStory(storyId, function (err, data, response) {
+            expect(data).to.be.an('array');
+            done();
+        });
+    });
+
+    it('should return at least one vote with a valid _id, story, userId and size', function (done) {
+        client.getVotesForStory(storyId, function (err, data, response) {
+            expect(data[data.length - 1]).to.deep.include({
+                _id: newVoteId,
+                story: storyId,
+                userId: newVote.userId,
+                size: newVote.size });
+            done();
+        });
+    });
+});
